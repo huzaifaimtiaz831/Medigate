faa512ad6ef60a18ad13153e269d547bbdb28c0c
huzaifaimtiaz831 <goldensupra94@gmail.com>
2025-10-26 21:25:38 +0500

    Add files via upload

---
+++ run.py
@@ -0,0 +1,9 @@
+print("Starting MediGate...")   # Debug print
+
+from medigate.gui import run_gui
+
+print("Import successful, now launching GUI...")
+
+if __name__ == "__main__":
+    print("Inside main, calling run_gui()...")
+    run_gui()
